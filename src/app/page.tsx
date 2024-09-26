495da1f6ddafa41061a0b1af6f8f6a3e62eccaba
James Marx <james@brewdev.co.za>
2024-09-26 22:42:45 +0200

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <>
       <div className="prose lg:prose-xl prose-h1:text-3xl mx-auto py-12 px-4">
-        <h1>Loan Repayment Calculator for Monzo Flex</h1>
+        <h1>Loan Payment Calculator for Monzo Flex</h1>
         <p className="underline">This is not an official Monzo product, and is not endorsed by them in any way.</p>
         <p>Noticed others are also searching for a flex calculator.</p>
         <p>As of today, my flex interest rate is 24% (and 0% if you pay within the first 3 months). Things could change anytime so please use the most current rate for the best estimates.</p>
